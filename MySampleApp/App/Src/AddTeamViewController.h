2cb563f817bed5daf5def5326e2b8263b384b69e
Taseen <tahseen0amin@gmail.com>
2015-10-29 22:20:40 +0530

    init

---
+++ MySampleApp/App/Src/AddTeamViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTeamViewController.h
+//  TasinMockApp
+//
+//  Created by Taseen Amin on 29/10/2015.
+//  Copyright © 2015 Amazon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTeamViewController : UIViewController
+
+@end
---
+++ MySampleApp/App/Src/AddTeamViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddTeamViewController.m
+//  TasinMockApp
+//
+//  Created by Taseen Amin on 29/10/2015.
+//  Copyright © 2015 Amazon. All rights reserved.
+//
+
+#import "AddTeamViewController.h"
+
+@interface AddTeamViewController ()
+
+@end
+
+@implementation AddTeamViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
